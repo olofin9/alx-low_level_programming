4d567702db8f4e33eec5060819a3e6b5623767ab
olofin taiwo <olofintaiwo11@gmail.com>
2023-03-23 07:41:06 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Correct)
+ */
+
+int main(void)
+
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
