ed74cde014100dd98df7fe0a7ccf4c4b67d02476
olofin taiwo <olofintaiwo11@gmail.com>
2023-03-23 07:44:08 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Correct)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%i", n);
+	printf("\n");
+	return (0);
+}
