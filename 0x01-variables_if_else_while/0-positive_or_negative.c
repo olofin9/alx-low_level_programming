46e8ca54a3c7dc71be0d958d8a03e5af5c70c132
olofin taiwo <olofintaiwo11@gmail.com>
2023-03-22 23:39:03 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Retun: Always 0 (correct)
+ */
+
+int main(void)
+{
+int n;
+srand(time(0);
+n =rand() - RAND_MAX / 2;
+
+if (n > 0)
+{
+printf("%i is zero\n", n);
+}
+
+else if (n == 0)
+{
+printf("%i is zero\n", n);
+}
+
+else
+{
+printf("%i is negative\n", n);
+}
+return (0);
+}
